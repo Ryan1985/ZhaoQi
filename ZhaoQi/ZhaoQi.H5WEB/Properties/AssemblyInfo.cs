c54b3186cd3bb9752468c5514ed578b22a8f249a
xieyuliang <xieyuliang1985@126.com>
2016-10-13 22:39:35 +0800

    [add]H5 webSite

---
+++ ZhaoQi/ZhaoQi.H5WEB/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ZhaoQi.H5WEB")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZhaoQi.H5WEB")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
