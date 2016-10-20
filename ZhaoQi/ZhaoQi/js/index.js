dc2421da5ae970b86f576d6a617f53a3f448228f
xieyuliang <xieyuliang1985@126.com>
2016-10-20 15:54:18 +0800

    upload

---
+++ ZhaoQi/ZhaoQi/js/index.js
@@ -0,0 +1,31 @@
+﻿"use strict";
+var pageViewApp = angular.module('pageView', ['ngRoute', 'ngAnimate']);
+
+pageViewApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            //templateUrl: 'RealTime.html',
+            controller: 'realTimeController'
+        })
+        .when('/Home/RealTime', {
+            //templateUrl: 'RealTime.html',
+            controller: 'realTimeController'
+        })
+        .when('/Home/HistoryData', {
+            //templateUrl: 'HistoryData.html',
+            controller: 'historyDataController'
+        });
+
+
+});
+
+
+pageViewApp.controller('realTimeController', ['$scope', function($scope) {
+    
+}]);
+
+
+pageViewApp.controller('historyDataController', ['$scope', function ($scope) {
+
+}]);
+
